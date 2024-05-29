f61aec75b889fa60ffe04ed4c52295dbc23b10d4
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2024-05-28 21:14:35 -0900

    final commit

--- script.js
+++ script.js
@@ -32,3 +32,56 @@ document.querySelectorAll('a').forEach(links => {
         cursor2.classList.remove('active');
     }
 })
+
+// javascript typing part
+
+document.addEventListener('DOMContentLoaded', function() {
+    const roles = ["Front-end developer","web designer", "graphic designer", "Video Editor", "computative programmer"];
+    let roleIndex = 0;
+    const roleElement = document.querySelector('.skills-list');
+    const typingSpeed = 100;
+    const erasingSpeed = 50;
+    const newTextDelay = 2000;
+
+    function typeRole() {
+        if (roleIndex < roles.length) {
+            const currentRole = roles[roleIndex];
+            roleElement.textContent = '';
+            let charIndex = 0;
+
+            function typeChar() {
+                if (charIndex < currentRole.length) {
+                    roleElement.textContent += currentRole.charAt(charIndex);
+                    charIndex++;
+                    setTimeout(typeChar, typingSpeed);
+                } else {
+                    setTimeout(eraseRole, newTextDelay);
+                }
+            }
+
+            typeChar();
+        }
+    }
+
+    function eraseRole() {
+        if (roleIndex < roles.length) {
+            const currentRole = roles[roleIndex];
+            let charIndex = currentRole.length;
+
+            function eraseChar() {
+                if (charIndex > 0) {
+                    roleElement.textContent = currentRole.substring(0, charIndex - 1);
+                    charIndex--;
+                    setTimeout(eraseChar, erasingSpeed);
+                } else {
+                    roleIndex = (roleIndex + 1) % roles.length;
+                    typeRole();
+                }
+            }
+
+            eraseChar();
+        }
+    }
+
+    typeRole();
+});
